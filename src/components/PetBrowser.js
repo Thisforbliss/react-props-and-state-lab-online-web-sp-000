1ce8791a40d261ab70aea9c7dc43d24a731d8c1c
Malcolm Leake <echelon1089@gmail.com>
2020-06-05 19:50:34 +0000

    Automatically backed up by Learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Pet from './Pet'
-// eslint-disable-next-line
+ eslint-disable-next-line
 class PetBrowser extends React.Component {
   render() {
     return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
